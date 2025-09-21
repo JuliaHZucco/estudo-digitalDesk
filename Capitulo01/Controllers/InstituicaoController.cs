60d405e0869dff46fee8db709e1bc9f6e584af7c
Júlia H. Zucco <juliahzucco@gmail.com>
2025-09-20 23:54:57 -0300

    Add project files.

---
+++ Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Capitulo01.Models;
+using System.Linq;
+
+namespace Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao() { InstituicaoID = 1, Nome = "UniParaná", Endereco = "Paraná" },
+            new Instituicao() { InstituicaoID = 2, Nome = "UniSanta", Endereco = "Santa Catarina"},
+            new Instituicao() { InstituicaoID = 3, Nome = "UniSãoPaulo", Endereco = "SãoPaulo" },
+            new Instituicao() { InstituicaoID = 4, Nome = "UniSulgrandense", Endereco = "Rio Grande do Sul"},
+            new Instituicao() { InstituicaoID = 5, Nome = "UniCarioca", Endereco = "Rio de Janeiro"}
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes.OrderBy(i => i.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            if (ModelState.IsValid)
+            {
+                instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+                instituicoes.Add(instituicao);
+                return RedirectToAction("Index");
+            }
+
+            return View(instituicao);
+        }
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            if (ModelState.IsValid)
+            {
+                instituicoes[instituicoes.IndexOf(
+                    instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First()
+                )] = instituicao;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(instituicao);
+        }
+        public ActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        public ActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            return RedirectToAction("Index");
+        }
+
+    }
+}
